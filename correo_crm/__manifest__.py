32644b72e86d7c5313a019827fdbfb0261628971
Carlos <carlosjmeilan@gmail.com>
2020-01-28 12:33:48 -0300

    6

--- correo_crm/__manifest__.py
+++ correo_crm/__manifest__.py
@@ -7,7 +7,7 @@
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        prueba conn 5
+        prueba conn 6
     """,
 
     'author': "Carlos Meilan",
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd ~/odoo-dev/11/proyecto1/src/addons-ar
+git pull origin master
+echo "deploy done"
