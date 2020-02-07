c2360ef04edff5470ed3cdc7ec82335d436efcb9
Carlos <carlosjmeilan@gmail.com>
2020-02-07 00:00:29 -0300

    correo_crm

--- correo_crm/models/models.py
+++ correo_crm/models/models.py
@@ -7,39 +7,30 @@ class correo_crm(models.Model):
      _inherit = 'crm.lead'
      correo_enviado = fields.Boolean(string='email enviado')
 
-     def automata(self):
-          mail_values = {
-              'subject' : 'CVS',
-              'body_html' : 'CVS',
-              'email_to':'carlosjmeilan@gmail.com',
-              'email_from':'admin@gestiongma.com'
-          }
-          self.env['mail.mail'].sudo().create(mail_values).send()
-
-
      @api.multi
-     def create(self, values):
+     def create(self, values,):
           record = super(correo_crm, self).create(values)
           record['correo_enviado'] = True
 
 
           email_to = values ['email_from']
-
-          body = '<![CDATA[
-	             <p>Dear ${(object.name)},<br/><br/>
-	             Good job, you've just created your first e-mail template!<br/></p>
-                 Regards,<br/>
-                ${(object.company_id.name)}
-	             ]]>'
+          name = values ['contact_name']
+          company_id = self.env['res.company'].search([('id','=', 1)])
+          company = company_id ['name']
+          email_from = company_id['email']
 
           mail_values = {
-             'subject' : 'gracias por comunicarse con nosotros',
-             'body_html': boby,
+             'subject' : 'Gracias por comunicarse con nosotros',
+             'body_html': '<p>Estimado ' + name + ', ' + 'gracias por comunicarse con nosotros </p> </br> </br>' + 'El equipo de ventas de  </br>' +  company,
              'email_to': email_to,
-             'email_from':'admin@gestiongma.com'
-              }
+             'email_from':email_from
+          }
 
           self.env['mail.mail'].sudo().create(mail_values).send()
+
+#          template = self.env.ref('auth_signup.mail_template_user_signup_account_created')
+#          template_id = template.id
+#          self.env['mail.template'].browse(template_id).send_mail(self.id)
 
 
           return record
